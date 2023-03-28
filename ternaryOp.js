30d329d6d00999dccd051bf7d570bebf0b6177c9
Marjan-001 <marjan199721@gmail.com>
2023-03-28 21:54:38 +0600

    ternary

---
+++ ternaryOp.js
@@ -0,0 +1,10 @@
+// condition?this part will execute if true : or this will 
+//basically it is a shortcut of if -else statement.
+
+let a = 20;
+let b = 30;
+
+let c = a < b ? 50 : 100;
+console.log(c);
+
+// here if a is less than b then the value of c will be 50 otherwise it will be 100
