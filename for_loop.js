9b33bec5df8f599d794bb1c458131db58b855e44
Marjan-001 <marjan199721@gmail.com>
2023-03-28 22:13:59 +0600

    for loop

---
+++ for_loop.js
@@ -0,0 +1,8 @@
+/* syntax for(initial value ; condition; step) >> for (var i =0; i<10; i++){statement;} 
+here the initial value of i is 0 and the loop will continue until the value of i is less than 10 and we 
+increment the value of i by one.
+*/
+
+for (var i = 0; i < 10; i++) {
+    console.log("Go" + i + "step");
+}
---
+++ if_else.js
@@ -0,0 +1,21 @@
+// find the max number 
+
+var a = 10;
+var b = 20;
+var c = 5;
+
+if (a > b && a > c) {
+
+    console.log("a is the largest number");
+
+}
+else if (b > a && b > c) {
+    console.log(`b ${b} is the largest number`);
+}
+else {
+    console.log("c is the largest number")
+}
+
+
+
+
